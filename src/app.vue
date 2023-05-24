039caa718ed16919608f7e387fb585bfbe6624b3
Shige Fukushima <shigeyf@outlook.com>
2023-05-25 01:15:06 +0900

    fix: 🐛 Removed some codes for testing

--- src/app.vue
+++ src/app.vue
@@ -1,13 +1,5 @@
-<script setup lang="ts">
-  import { mdiAccount } from '@mdi/js'
-</script>
-
 <template>
   <div>
-    <v-container>
-      <v-btn color="primary">Primary</v-btn>
-      <v-icon :icon="mdiAccount"></v-icon>
-    </v-container>
     <NuxtWelcome />
   </div>
 </template>
